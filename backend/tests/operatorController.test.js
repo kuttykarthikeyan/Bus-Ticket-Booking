dbd23986d9ac6817fc5cf72242376c7a6f4837ce
Karthikeyan S <121245383+kuttykarthikeyan@users.noreply.github.com>
2025-03-25 18:22:49 +0530

    tests

--- backend/tests/operatorController.test.js
+++
@@ -1,8 +0,0 @@
-
-jest.mock("../utils/authUtils.js", () => ({
-  hashPassword: jest.fn(() => "hashed_password"),
-}));
-
-test("should hash password", () => {
-  expect(require("../utils/authUtils.js").hashPassword()).toBe("hashed_password");
-});
